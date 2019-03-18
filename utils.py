59df562f2d51b00aaae5f0463d3c8d594645c8c5
olaf-skrabacz <olafsk123@gmail.com>
2019-03-18 16:33:59 +0100

    add utils with plot-decicsion-surface function

---
+++ utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_decision_surface(nn, X, n_samples = 50, proba=True, **kwargs):
+    OX = np.linspace(X['x'].min(), X['x'].max(), n_samples)
+    OY = np.linspace(X['x'].min(), X['y'].max(), n_samples)
+    xx, yy = np.meshgrid(OX, OY)
+    flat_x, flat_y = xx.flatten().tolist(), yy.flatten().tolist()
+    z = np.zeros_like(flat_x)
+    for i in range(len(flat_x)):
+        z[i] = nn.forward_pass(np.array([[flat_x[i], flat_y[i]]]))
+    if not proba:
+        z = z > 0.5
+    plt.contourf(xx,yy, z.reshape(xx.shape[0],xx.shape[1]),
+    **kwargs)
+    return z
